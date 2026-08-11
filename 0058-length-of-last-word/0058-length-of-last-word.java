c9e6271028dad4caa359dd94427b7ace5de75193
Samiksha kumari <samikshakumari461@gmail.com>
2026-08-12 01:06:30 +0530

    Time: 0 ms (100%), Space: 43.1 MB (52.29%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n= s.length();
+        int c=0;
+        for(int i=n-1;i>=0;i--){
+            if(s.charAt(i)==' '){
+                if(c>0) break;
+            }
+            else c++;
+        }
+        return c;
+        
+    }
+}
